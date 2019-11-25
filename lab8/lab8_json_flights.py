0ff039590f4ed1fba97fa7e7732cea80a2927177
devedzic <devedzic@gmail.com>
2019-11-25 01:19:29 +0100

    Lab 8, start

---
+++ lab8/lab8_json_flights.py
@@ -0,0 +1,28 @@
+# The starting point for this task is the module lab6_flight, that is, the Flight class.
+# Note that one attribute of this class is a list of flight passengers, that is, a list
+# of objects of the class Passenger and/or its subclasses BusinessPassenger and
+# EconomyPassenger defined in the module lab6_passengers.
+# In addition, one attribute (departure) is of the datetime.datetime type.
+# Your task is to create functions for
+# - serialising (writing) objects of the class Flight into a .json file
+# - deserializing (reconstructing) objects of the class Flight from a
+#   .json file (created using the previous function)
+
+
+
+if __name__ == '__main__':
+
+	pass
+
+    # lh992 = Flight.from_Frankfurt_by_Lufthansa('LH992', '2019-12-03 12:20')
+    # lh992.destination = "Amsterdam"
+    
+
+    # bob = BusinessPassenger("Bob Smith", "123456", air_miles=1000, checked_in=True)
+    # john = EconomyPassenger("John Smith", "987654", checked_in=False)
+    # bill = EconomyPassenger("Billy Stone", "917253", air_miles=5000, checked_in=True)
+    # dona = EconomyPassenger("Dona Stone", "917254", air_miles=2500, checked_in=True)
+    # kate = EconomyPassenger("Kate Fox", "114252", air_miles=3500, checked_in=True)
+
+    # for p in (bob, john, bill, dona, kate):
+      #   lh992.add_passenger(p)
---
+++ lab8/lab8_json_passengers.py
@@ -0,0 +1,27 @@
+# The starting point for this task is the module lab6_passengers, that is,
+# the Passenger class and its subclasses BusinessPassenger and EconomyPassenger.
+# Your task is to create functions for
+# - serialising (writing) objects of the class Passenger and its subclasses
+#   into a .json file
+# - deserializing (reconstructing) objects of the class Passenger and its subclasses
+#   from a .json file (created using the previous function)
+
+
+
+
+if __name__ == '__main__':
+
+	pass
+	
+    # bob = BusinessPassenger("Bob Smith", "123456", air_miles=1000, checked_in=True)
+    # john = EconomyPassenger("John Smith", "987654", checked_in=False)
+    # bill = EconomyPassenger("Billy Stone", "917253", air_miles=5000, checked_in=True)
+    # dona = EconomyPassenger("Dona Stone", "917253", air_miles=2500, checked_in=True)
+    
+    # passengers = [bob, john, bill, dona]
+    # for p in passengers:
+    #     print(p)
+    #     print(p.__dict__)
+    #     print()
+
+
